291b4fe3a0529351f3cc95214fc733f8d83d9528
tanlucdev <tanluc.dev@gmail.com>
2024-05-13 17:07:38 +0700

    change logo in logosearch component

--- client/src/components/LogoSearch/LogoSearch.jsx
+++ client/src/components/LogoSearch/LogoSearch.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Logo from '../../img/logo.png'
+import Logo from './logo.png'
 import { UilSearch } from '@iconscout/react-unicons'
 import './LogoSearch.scss'
 function LogoSearch() {
   return (
     <div className="LogoSearch">
-      {/* <img src={Logo} alt="" /> */}
+      <img src={Logo} alt="" />
       <div className="Search">
         <input type="text" placeholder="#Explore" />
         <div className="s-icon">
