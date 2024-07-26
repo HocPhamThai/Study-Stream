d14c2ee1f2889e01e2e19734776875d1ee352081
tanlucdev <tanluc.dev@gmail.com>
2024-07-27 01:34:44 +0700

    Add feature table song

---
+++ client/src/components/AdminManageMusic/DeleteSongModal.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import axios from 'axios';
+import '../UserTable/UserTable.scss'
+import { toast } from 'sonner'
+
+const DeleteSongModal = ({ isOpen, onClose, songId, nameSong, onSongDeleted }) => {
+  if (!isOpen) return null;
+  const handleDelete = async () => {
+    try {
+      const response = await axios.delete(`http://localhost:8001/songs/${songId}`)
+      console.log(response.data.message); // Xử lý thông báo thành công
+
+      onSongDeleted(songId, nameSong)
+      onClose(); // Đóng modal sau khi xóa thành công
+    } catch (error) {
+      if (error.response) {
+        console.error(error.response.data.message); // Xử lý thông báo lỗi từ server
+      } else {
+        console.error('An error occurred:', error.message); // Xử lý lỗi không liên quan đến server
+      }
+    }
+  }
+
+  return (
+    <div
+      id="deleteModal"
+      tabIndex="-1"
+      aria-hidden="true"
+      className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden"
+    >
+      <div className="relative p-4 w-full max-w-md h-full md:h-auto">
+        {/* Modal content */}
+        <div className="relative p-4 text-center bg-white rounded-lg shadow sm:p-5">
+          <button
+            type="button"
+            className="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
+            onClick={onClose}
+          >
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span className="sr-only">Close modal</span>
+          </button>
+          <svg
+            className="text-gray-400 w-11 h-11 mb-3.5 mx-auto"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <p className="mb-4 text-gray-500">
+            Are you sure you want to delete this song?
+          </p>
+          <div className="flex justify-center items-center space-x-4">
+            <button
+              type="button"
+              className="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10"
+              onClick={onClose}
+            >
+              No, cancel
+            </button>
+            <button
+              type="button"
+              className="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300"
+              onClick={handleDelete}
+            >
+              Yes, I'm sure
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteSongModal
---
+++ client/src/components/AdminManageMusic/UpdateSongModal.jsx
@@ -0,0 +1,147 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios'
+import { toast } from 'sonner'
+import '../UserTable/UserTable.scss'
+
+function UpdateSongModal({ data, onSongUpdated }) {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [formData, setFormData] = useState(data)
+  const dispatch = useDispatch()
+  const loading = useSelector((state) => state.authReducer.loading)
+  const error = useSelector((state) => state.authReducer.error)
+  const initialData = useRef(data);
+
+  // Hàm so sánh dữ liệu
+  const isDataChanged = (newData, oldData) => {
+    return JSON.stringify(newData) !== JSON.stringify(oldData);
+  };
+
+  useEffect(() => {
+    // Cập nhật formData khi data thay đổi thực sự
+    if (isDataChanged(data, initialData.current)) {
+      setFormData(data);
+      initialData.current = data; // Cập nhật dữ liệu ban đầu
+    }
+  }, [data]);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Gửi yêu cầu POST tới API
+      const response = await axios.put(`http://localhost:8001/songs/${data._id}`, formData);
+
+      // Gọi hàm onSongUpdated với dữ liệu người dùng đã cập nhật
+      onSongUpdated(response.data)
+    } catch (error) {
+      console.error("Error updating song: ", error.response ? error.response.data : error.message);
+    } finally {
+      // Đóng modal sau khi cập nhật thành công hoặc thất bại
+      setIsModalOpen(false);
+    }
+  };
+  const openModal = () => setIsModalOpen(true)
+  const closeModal = () => setIsModalOpen(false)
+
+  return (
+    <div>
+      <button
+        type="button"
+        data-drawer-target="drawer-update-product"
+        data-drawer-show="drawer-update-product"
+        aria-controls="drawer-update-product"
+        className="py-2 px-3 flex items-center text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300600 "
+        onClick={openModal}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4 mr-2 -ml-0.5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+        >
+          <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+          <path
+            fillRule="evenodd"
+            d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+            clipRule="evenodd"
+          />
+        </svg>
+        Edit
+      </button>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="fixed inset-0 bg-gray-600 bg-opacity-75"></div>
+          <div className="bg-gray-800 rounded-lg shadow-lg p-8 z-50 w-full max-w-2xl">
+            <h1 className='font-bold text-[20px] mb-5 text-white'>Add new song</h1>
+            <hr className='mb-5' />
+            <form onSubmit={handleSubmit}>
+              <div className="grid gap-4 mb-4 sm:grid-cols-2">
+                <div>
+                  <label
+                    htmlFor="firstname"
+                    className="block mb-2 text-sm font-medium text-white "
+                  >
+                    Song Name
+                  </label>
+                  <input
+                    type="text"
+                    name="nameSong"
+                    id="nameSong"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                    placeholder="Enter song name..."
+                    onChange={handleChange}
+                    value={formData.nameSong}
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="description"
+                    className="block mb-2 text-sm font-medium text-white "
+                  >
+                    Link of song
+                  </label>
+                  <textarea
+                    type="text"
+                    name="linkStored"
+                    id="linkStored"
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 "
+                    placeholder="Enter link of song here"
+                    onChange={handleChange}
+                    value={formData.linkStored}
+                    required
+                  />
+                </div>
+
+              </div>
+              <div className="flex justify-end space-x-2">
+                <button
+                  type="button"
+                  onClick={closeModal}
+                  className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 border border-gray-200 rounded-lg text-sm font-medium px-5 py-2.5 hover:text-primary-900 focus:z-10 "
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className="text-white bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                >
+                  Add Song
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default UpdateSongModal
