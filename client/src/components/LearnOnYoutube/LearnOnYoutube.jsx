a4474a5cad330a96a81790418b3e632e2153afdd
Hoc Pham <phamthaihoc008@gmail.com>
2024-08-19 12:17:59 +0700

    edit UI

--- client/src/components/LearnOnYoutube/LearnOnYoutube.jsx
+++ client/src/components/LearnOnYoutube/LearnOnYoutube.jsx
@@ -1,50 +1,52 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const LearnOnYoutube = () => {
-  const [inputValue, setInputValue] = useState('');
-  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState('')
+  const navigate = useNavigate()
 
   const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
+    setInputValue(e.target.value)
+  }
 
   const getId = (url) => {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-    const match = url.match(regExp);
+    const regExp =
+      /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/
+    const match = url.match(regExp)
 
     if (match && match[2].length === 11) {
-      return match[2];
+      return match[2]
     } else {
-      return 'error';
+      return 'error'
     }
-  };
+  }
 
   const handleSubmit = () => {
-    const videoId = getId(inputValue);
+    const videoId = getId(inputValue)
     if (videoId !== 'error') {
-      const embedLink = `https://www.youtube.com/embed/${videoId}`;
-      console.log('Embed Link:', embedLink); // Kiểm tra link embed
-      navigate('/study_pagewith_youtube', { state: { embedLink } }); // Truyền dữ liệu và điều hướng
+      const embedLink = `https://www.youtube.com/embed/${videoId}`
+      console.log('Embed Link:', embedLink) // Kiểm tra link embed
+      navigate('/study_pagewith_youtube', { state: { embedLink } }) // Truyền dữ liệu và điều hướng
     } else {
-      alert('Invalid YouTube URL');
+      alert('Invalid YouTube URL')
     }
-  };
+  }
 
   return (
-    <div className="learn-on-youtube-container">
+    <div className="learn-on-youtube-container mt-4 flex flex-col bg-white rounded-lg p-4">
+      <h1 className="text-md text-gray-800 font-bold">Learn on YouTube</h1>
       <input
         type="text"
         value={inputValue}
         onChange={handleInputChange}
         placeholder="Enter YouTube URL"
-        className="input-field"
+        className="input-field p-3 rounded-lg mt-2"
       />
-      <button onClick={handleSubmit} className="submit-button">
+      <button onClick={handleSubmit} className="submit-button button mt-2 p-2">
         Convert & Study
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default LearnOnYoutube;
+export default LearnOnYoutube
